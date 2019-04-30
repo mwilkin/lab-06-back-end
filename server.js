e6072a06bfc1e44551ce2f697ee02f7edf213191
Matt Wilkin <mwilkin22@gmail.com>
2019-04-30 10:42:36 -0700

    constructor function completed

--- server.js
+++ server.js
@@ -24,26 +24,33 @@ app.get('/location', (request, response) => {
   }
 });
 
-//Helper Function
-function searchToLatLong(query) {
+// //Helper Function
+// function searchToLatLong(query) {
+// if(!query) throw new Error('No query sent');
+//   const geoData =require('./data/geo.json');
+//   const location = {
+//     search_query: query,
+//     formatted_query: geoData.results[0].formatted_address,
+//     latitude: geoData.results[0].geometry.location.lat,
+//     longitude: geoData.results[0].geometry.location.lng
+//   };
+//   return location;
+// }
+
+//Refactor
+function Location(query, geoData){
+  this.search_query = query;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+
   if(!query) throw new Error('No query sent');
-  const geoData =require('./data/geo.json');
-  const location = {
-    search_query: query,
-    formatted_query: geoData.results[0].formatted_address,
-    latitude: geoData.results[0].geometry.location.lat,
-    longitude: geoData.results[0].geometry.location.lng
-  };
-  return location;
+
 }
 
-// //Refactor
-// function Location(query, geoData){
-//   this.search_query = query;
-//   this.formatted_query = geoData.results[0].formatted_address;
-//   this.latitude = geoData.results[0].geometry.location.lat;
-//   this.longitude = geoData.results[0].geometry.location.lng;
-// }
+
+
+
 
 //Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
